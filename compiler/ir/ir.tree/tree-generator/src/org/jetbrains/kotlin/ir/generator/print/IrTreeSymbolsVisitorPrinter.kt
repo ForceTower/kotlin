a0e2183233d7273495fbadfc495870c2e81979e2
Francesco Protopapa <fprotopapa@google.com>
2026-06-08 11:00:04 +0200

    [IR] Visit `reflectionTargetSymbol` in `IrTreeSymbolVisitor`

--- compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/print/IrTreeSymbolsVisitorPrinter.kt
+++ compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/print/IrTreeSymbolsVisitorPrinter.kt
@@ -15,6 +15,8 @@ import org.jetbrains.kotlin.ir.generator.IrTree.functionReference
 import org.jetbrains.kotlin.ir.generator.IrTree.localDelegatedPropertyReference
 import org.jetbrains.kotlin.ir.generator.IrTree.propertyReference
 import org.jetbrains.kotlin.ir.generator.deprecatedCompilerApi
+import org.jetbrains.kotlin.ir.generator.IrTree.richFunctionReference
+import org.jetbrains.kotlin.ir.generator.IrTree.richPropertyReference
 import org.jetbrains.kotlin.ir.generator.model.Element
 import org.jetbrains.kotlin.ir.generator.model.Field
 import org.jetbrains.kotlin.ir.generator.model.ListField
@@ -96,6 +98,8 @@ internal class IrTreeSymbolsVisitorPrinter(
             functionReference -> println("visitReferencedFunction(expression, expression.symbol)")
             propertyReference -> println("visitReferencedProperty(expression, expression.symbol)")
             localDelegatedPropertyReference -> println("visitReferencedLocalDelegatedProperty(expression, expression.symbol)")
+            richFunctionReference -> println("expression.reflectionTargetSymbol?.let { visitReferencedFunction(expression, it) }")
+            richPropertyReference -> println("expression.reflectionTargetSymbol?.let { visitReferencedDeclarationWithAccessors(expression, it) }")
         }
     }
 
